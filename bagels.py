daa3a9345116d6fae983c4d4327ee18663960973
Mustapha Seddoum <76795264+Amiral-Code@users.noreply.github.com>
2024-01-30 17:16:18 +0100

    Update TSF_Game.py

--- bagels.py
+++ bagels.py
@@ -1,95 +1,101 @@
-"""TCF, by Amiral7YT 
-A deductive logic game where you must guess a number based on clues.
-This code is available at https://
-Tags: short, game, puzzle"""
-
 import random
 
-NUM_DIGITS = 4  # (!) Try setting this to 1 or 10.
-MAX_GUESSES = 10  # (!) Try setting this to 1 or 100.
+def get_num_digits():
+    while True:
+        try:
+            num_digits = int(input("How many digits do you want to be in the secret number: "))
+            if 1 <= num_digits <= 9:
+                return num_digits
+            else:
+                print("Please enter a number between 1 and 9.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
+def get_max_guesses():
+    while True:
+        try:
+            max_guesses = int(input("How many guesses do you want to have: "))
+            if 1 <= max_guesses <= 100:
+                return max_guesses
+            else:
+                print("Please enter a number between 1 and 100.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
+def display_rules():
+    with open('TSF_Game_Rules.txt', 'r') as file:
+        rules = file.read()
+
+    answer = input("Do you want to see the rules of the TSF game? (yes/no): ")
+
+    if answer.lower().startswith('y'):
+        print(rules)
+    else:
+        print("Let's continue with the game!")
+
+def get_secret_num(num_digits):
+    secret_num = random.sample(range(0, 9), num_digits)
+    return secret_num
+
+def get_player_guess(num_digits, num_guesses, max_guesses):
+    while num_guesses <= max_guesses:
+        player_guess = input(f"Enter a guess with {num_digits} unique numbers: #{num_guesses} ")
+
+        if len(player_guess) != num_digits or not player_guess.isdigit():
+            print(f"Please enter a valid {num_digits}-digit number.")
+            continue
+
+        unique_numbers = set(player_guess)
+        if len(unique_numbers) != len(player_guess):
+            print("Please make sure the numbers are unique.")
+            continue
+
+        return list(player_guess)
+
+def get_clues(player_guess, secret_num):
+    clues = []
+    for i in range(len(player_guess)):
+        if str(player_guess[i]) == str(secret_num[i]):
+            clues.append('T')
+        elif str(player_guess[i]) in str(secret_num):
+            clues.append('S') 
+        else:
+            clues.append('F')
+    return clues
 
 def main():
-    print('''TCF, a deductive logic game.
-By Amiral7YT
+    # Ask the player if he want to see the rules.
+    display_rules()
+    while True:
+        num_digits = get_num_digits()
+        max_guesses = get_max_guesses()
 
-I am thinking of a {}-digit number with no repeated digits.
-Try to guess what it is. Here are some clues:
-When I say: That means:
-     T      That digit is correct and in the right position.
-     C      That digit is correct but in the wrong position.
-     F      That digit is incorrect.
+        secret_num = get_secret_num(num_digits)
+        print(f'I have thought up a {num_digits} digit number.')
+        print(f'You have {max_guesses} guesses to get it.')
+        num_guesses = 1
 
-For example, if the secret number was 2485 and your guess was 8436, the
-clues would be ['F', 'T', 'C', 'F'].'''.format(NUM_DIGITS))
+        while True:
+            player_guess = get_player_guess(num_digits, num_guesses, max_guesses)
+            print(str(player_guess))
+            num_guesses += 1
 
-    while True:  # Main game loop.
-        # This stores the secret number the player needs to guess:
-        secretNum = random.sample(range(0, 9), 4)
-        print('I have thought up a {} digit number.'.format(NUM_DIGITS))
-        print(' You have {} guesses to get it.'.format(MAX_GUESSES))
-
-        numGuesses = 1
-        while numGuesses <= MAX_GUESSES:
-            guess = [ ]
-            # Keep looping until they enter a valid guess:
-            while len(guess) != NUM_DIGITS :
-                print('Guess #{}: '.format(numGuesses))
-                while len(guess) != NUM_DIGITS :
-                    guess = input('Guess > ')
-                    if len(guess) != NUM_DIGITS :
-                        guess = input('Guess > ')
-                    elif len(guess) == NUM_DIGITS:
-                        guess = [int(x) for i,x in enumerate(guess)]
-                        #Make sure there is no reapeted numbers
-                        dup = [ ]
-                        for i in guess :                           
-                            if i not in dup :
-                                dup.append(i)
-                            else:
-                                dup.clear()
-                                guess = input('Guess > ')
-                guess = [int(x) for i,x in enumerate(guess)]
-            clues = getClues(guess, secretNum)
+            clues = get_clues(player_guess, secret_num)
             print(clues)
-            numGuesses += 1
 
-            if clues == ['T', 'T', 'T', 'T']:
+            if set(clues) == {'T'}:
                 print('You got it!')
-                break  # They're correct, so break out of this loop.
-            if numGuesses > MAX_GUESSES:
-                print('You ran out of guesses.')
-                print('The answer was {}.'.format(secretNum))
+                break
 
-        # Ask player if they want to play again.
+            if num_guesses > max_guesses:
+                print('You ran out of guesses.')
+                print(f'The answer was {secret_num}.')
+                break
+
         print('Do you want to play again? (yes or no)')
         if not input('> ').lower().startswith('y'):
+            print('Thanks for playing!')
             break
-    print('Thanks for playing!')
 
-
-def getClues(guess, secretNum):
-    
-    clues = [ ]
-
-    for i in range(len(guess)):
-        if guess[i] == secretNum[i]:
-            # A correct digit is in the correct place.
-            clues.append('T')
-        elif guess[i] in secretNum:
-            # A correct digit is in the incorrect place.
-            clues.append('C')
-        else :
-            clues.append('F')     
-    # Make a single string from the list of string clues.
-    return clues
-    """Returns a string with the (T)rue, (C)hange, (F)alse clues for guess
-    and secret number pair."""
-    if clues == ['T', 'T', 'T', 'T']:
-        print('You got it!')
-        return
-    
-
-# If the program is run (instead of imported), run the game:
 if __name__ == '__main__':
     main()
